be3e279dd47c96ad9f911a5d314013400d5b91f8
Rahul Mahadev <rahul.mahadev7@gmail.com>
2019-02-26 23:32:34 -0600

    Added db integration- leveldb

--- plugin.go
+++ plugin.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 // New returns an implementation of the chaincode interface.
@@ -27,7 +29,6 @@ func (s *honeybadgerscc) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	}
 
 	// Set up any variables or assets here by calling stub.PutState()
-
 	// We store the key and the value on the ledger
 	err := stub.PutState(args[0], []byte(args[1]))
 	if err != nil {
@@ -37,15 +38,49 @@ func (s *honeybadgerscc) Init(stub shim.ChaincodeStubInterface) pb.Response {
 }
 
 // StartHbmpc start honeybadgerMPC
-func StartHbmpc(nodeID string) {
+func StartHbmpc(nodeID string) string {
 	cmd := exec.Command("python3.7", "-m", "honeybadgermpc.secretshare_hbavsslight", nodeID, "conf/hbavss.hyper.ini")
 	cmd.Dir = "/usr/src/HoneyBadgerMPC"
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
 	errmsg := cmd.Run()
 	if errmsg != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", errmsg)
 	}
+	lines := strings.Split(outb.String(), "\n")
+	for _, line := range lines {
+		if strings.Contains(line, "[INFO]: Output available ") {
+			// Very hacky way of doing this.
+			shareParts := strings.Split(line, "[INFO]: Output available ")
+			if len(shareParts) >= 2 {
+				share := shareParts[1]
+				fmt.Println("The share is ", share)
+				return share
+			}
+		}
+	}
+	return "None"
+	// fmt.Println("out : ", outb.String(), "err: ", errb.String())
+}
+
+func dbPut(key string, value string) {
+	db, err := leveldb.OpenFile("db", nil)
+	err = db.Put([]byte(key), []byte(value), nil)
+	if err != nil {
+		fmt.Println("Error writing to database")
+	}
+	defer db.Close()
+}
+
+func dbGet(key string) string {
+	db, err := leveldb.OpenFile("db", nil)
+	data, err := db.Get([]byte(key), nil)
+	if err != nil {
+		fmt.Println("Error getting from database")
+	}
+	defer db.Close()
+	return string(data)
 }
 
 // Invoke implements the chaincode shim interface
@@ -57,10 +92,32 @@ func (s *honeybadgerscc) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		result, err = set(stub, args)
 	} else if fn == "hbmpc" && len(args) >= 1 {
 		fmt.Println("[honeybadgerscc]HBMPC")
-		StartHbmpc(args[0])
+		share := StartHbmpc(args[0])
+		if share != "None" {
+			err := stub.PutState("key", []byte(share))
+			dbPut("key", share)
+			if err != nil {
+				fmt.Errorf("Failed to set asset: %s", args[0])
+			}
+		}
+		//err = db.Put([]byte("key"), []byte(share), nil)
+		//if err != nil {
+		//	fmt.Println("There has been an error writing to db")
+		//	return shim.Error(err.Error())
+		//}
 		return shim.Success([]byte("Started HBMPC-hbavss"))
-	} else { // assume 'get' even if fn is nil
+	} else if fn == "get" { // assume 'get' even if fn is nil
 		result, err = get(stub, args)
+	} else if fn == "test" {
+		fmt.Println("[honetbadgerscc] I am here starting ")
+	} else if fn == "getKey" {
+		share := dbGet("key")
+		//data, err := db.Get([]byte("key"), nil)
+		//if err != nil {
+		//	fmt.Println("error getting data ")
+		//}
+		//fmt.Println(data)
+		return shim.Success([]byte(share))
 	}
 	if err != nil {
 		return shim.Error(err.Error())
--- script.sh
+++ script.sh
@@ -2,5 +2,7 @@ docker run -i --rm  -v $GOPATH/src/github.com/hybridNeo/honeybadgerscc:/opt/gopa
 		-v /Users/rahulshivumahadev/go/src/github.com/hyperledger/fabric:/opt/gopath/src/github.com/hyperledger/fabric \
                 -v /Users/rahulshivumahadev/go/src/github.com/hybridNeo/honeybadgerscc/.build/docker/bin:/opt/gopath/bin \
                 -v /Users/rahulshivumahadev/go/src/github.com/hybridNeo/honeybadgerscc/.build/docker/pkg:/opt/gopath/pkg \
+                -v /Users/rahulshivumahadev/go/src/github.com/syndtr/goleveldb:/opt/gopath/src/github.com/syndtr/goleveldb \
+                -v /Users/rahulshivumahadev/go/src/github.com/golang/snappy:/opt/gopath/src/github.com/golang/snappy \
                 hyperledger/fabric-baseimage:amd64-0.4.14 \
                 go build -buildmode=plugin -tags ""
