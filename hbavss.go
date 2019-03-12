cebd0ac5aec178d049fbc4c317faca4d9aab92e9
Rahul Mahadev <rahul.mahadev7@gmail.com>
2019-03-12 02:05:53 -0500

    Modified to accomodate public reconstruction

--- hbavss.go
+++ hbavss.go
@@ -37,3 +37,54 @@ func StartHbmpc(nodeID string) string {
 	return "None"
 	// fmt.Println("out : ", outb.String(), "err: ", errb.String())
 }
+
+// InitiatePubRec initiates public reconstruction
+func InitiatePubRec(key string, namespace string) string {
+	cmd := exec.Command("python3.7", "-m", "honeybadgermpc.fabric_public_reconstruct", key, namespace)
+	cmd.Dir = "/usr/src/HoneyBadgerMPC"
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+	errmsg := cmd.Run()
+	if errmsg != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", errmsg)
+	}
+	fmt.Println(cmd.Stdout)
+	fmt.Println(cmd.Stderr)
+	return "None"
+}
+
+// StartPubRec start public reconstruction
+func StartPubRec(nodeID string, share string) string {
+	cmd := exec.Command("python3.7", "-m", "honeybadgermpc.public_reconstruct", nodeID, "conf/hbavss.hyper.ini", share)
+	cmd.Dir = "/usr/src/HoneyBadgerMPC"
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+	errmsg := cmd.Run()
+	if errmsg != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", errmsg)
+	}
+	lines := strings.Split(outb.String(), "\n")
+	for _, line := range lines {
+		if strings.Contains(line, "Reconstructed Value:") {
+			// Very hacky way of doing this.
+			secretParts := strings.Split(line, "Reconstructed Value:")
+			if len(secretParts) >= 2 {
+				secret := secretParts[1]
+				fmt.Println("The reconstructed secret is  is ", secret)
+				return secret
+			}
+		}
+	}
+	return "None"
+	// fmt.Println("out : ", outb.String(), "err: ", errb.String())
+
+}
+
+// TODO
+// contact namespace associated with chaincode and authorize user
+func authenticateRequest(namespace string, key string) bool {
+	fmt.Println("[honeybadgerscc] Authenticating request")
+	return true
+}
--- plugin.go
+++ plugin.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"log"
-	"os/exec"
-	"strings"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
-	"github.com/syndtr/goleveldb/leveldb"
 )
 
 // New returns an implementation of the chaincode interface.
 func New() shim.Chaincode {
-	fmt.Println("[honeybadgerscc] Hello")
+	fmt.Println("[honeybadgerscc] Chaincode Started.")
 	return &honeybadgerscc{}
 }
 
@@ -34,133 +29,67 @@ func (s *honeybadgerscc) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	if err != nil {
 		return shim.Error(fmt.Sprintf("Failed to create asset: %s", args[0]))
 	}
+
+	err = stub.PutState("readToken", []byte("0"))
+	if err != nil {
+		return shim.Error("Unable to set readToken")
+	}
 	return shim.Success(nil)
-}
-
-// StartHbmpc start honeybadgerMPC
-func StartHbmpc(nodeID string) string {
-	cmd := exec.Command("python3.7", "-m", "honeybadgermpc.secretshare_hbavsslight", nodeID, "conf/hbavss.hyper.ini")
-	cmd.Dir = "/usr/src/HoneyBadgerMPC"
-	var outb, errb bytes.Buffer
-	cmd.Stdout = &outb
-	cmd.Stderr = &errb
-	errmsg := cmd.Run()
-	if errmsg != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", errmsg)
-	}
-	lines := strings.Split(outb.String(), "\n")
-	for _, line := range lines {
-		if strings.Contains(line, "[INFO]: Output available ") {
-			// Very hacky way of doing this.
-			shareParts := strings.Split(line, "[INFO]: Output available ")
-			if len(shareParts) >= 2 {
-				share := shareParts[1]
-				fmt.Println("The share is ", share)
-				return share
-			}
-		}
-	}
-	return "None"
-	// fmt.Println("out : ", outb.String(), "err: ", errb.String())
-}
-
-func dbPut(key string, value string) {
-	db, err := leveldb.OpenFile("db", nil)
-	err = db.Put([]byte(key), []byte(value), nil)
-	if err != nil {
-		fmt.Println("Error writing to database")
-	}
-	defer db.Close()
-}
-
-func dbGet(key string) string {
-	db, err := leveldb.OpenFile("db", nil)
-	data, err := db.Get([]byte(key), nil)
-	if err != nil {
-		fmt.Println("Error getting from database")
-	}
-	defer db.Close()
-	return string(data)
 }
 
 // Invoke implements the chaincode shim interface
 func (s *honeybadgerscc) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	fn, args := stub.GetFunctionAndParameters()
-	var result string
 	var err error
-	if fn == "set" {
-		result, err = set(stub, args)
-	} else if fn == "hbmpc" && len(args) >= 1 {
+	if fn == "hbmpc" && len(args) >= 3 {
 		fmt.Println("[honeybadgerscc]HBMPC")
+		key := args[1]
+		namespace := args[2]
+		if !authenticateRequest(namespace, key) {
+			return shim.Success([]byte("Failed to authenticate request"))
+		}
 		share := StartHbmpc(args[0])
 		if share != "None" {
-			err := stub.PutState("key", []byte(share))
-			dbPut("key", share)
+			err := stub.PutState(key, []byte("NA"))
+			dbPut(key, share)
 			if err != nil {
 				fmt.Errorf("Failed to set asset: %s", args[0])
 			}
 		}
-		//err = db.Put([]byte("key"), []byte(share), nil)
-		//if err != nil {
-		//	fmt.Println("There has been an error writing to db")
-		//	return shim.Error(err.Error())
-		//}
-		return shim.Success([]byte("Started HBMPC-hbavss"))
-	} else if fn == "get" { // assume 'get' even if fn is nil
-		result, err = get(stub, args)
-	} else if fn == "test" {
-		fmt.Println("[honetbadgerscc] I am here starting ")
-	} else if fn == "getKey" {
-		share := dbGet("key")
-		//data, err := db.Get([]byte("key"), nil)
-		//if err != nil {
-		//	fmt.Println("error getting data ")
-		//}
-		//fmt.Println(data)
+		return shim.Success([]byte("Ran HBMPC-hbavss"))
+	} else if fn == "getKey" && len(args) >= 1 {
+		key := args[0]
+		share := dbGet(key)
 		return shim.Success([]byte(share))
+	} else if fn == "pubRecon" && len(args) >= 2 {
+		nodeID := args[0]
+		key := args[1]
+		share := dbGet(key)
+		value := StartPubRec(nodeID, share)
+		err := stub.PutState(key, []byte(value))
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		return shim.Success([]byte(value))
+	} else if fn == "initRecon" && len(args) >= 2 {
+		key := args[0]
+		namespace := args[0]
+		res := InitiatePubRec(key, namespace)
+		return shim.Success([]byte(res))
+	} else if fn == "get" && len(args) > 1 {
+		key := args[0]
+		value, err := stub.GetState(key)
+		if err != nil {
+			return shim.Success([]byte("Key does not exist yet"))
+		}
+		return shim.Success([]byte(value))
 	}
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
 	// Return the result as success payload
-	return shim.Success([]byte(result))
-}
-
-// Set stores the asset (both key and value) on the ledger. If the key exists,
-// it will override the value with the new one
-func set(stub shim.ChaincodeStubInterface, args []string) (string, error) {
-	if len(args) != 2 {
-		return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
-	}
-	v, e := stub.GetState("a")
-	if e != nil {
-		fmt.Println("ERRORR")
-	} else {
-		fmt.Println("Not err", v)
-	}
-	fmt.Println("value is " + args[1])
-	err := stub.PutState(args[0], []byte(args[1]))
-	if err != nil {
-		return "", fmt.Errorf("Failed to set asset: %s", args[0])
-	}
-	return args[1], nil
-}
-
-// Get returns the value of the specified asset key
-func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
-	if len(args) != 1 {
-		return "", fmt.Errorf("Incorrect arguments. Expecting a key")
-	}
-
-	value, err := stub.GetState(args[0])
-	if err != nil {
-		return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
-	}
-	if value == nil {
-		return "", fmt.Errorf("Asset not found: %s", args[0])
-	}
-	return string(value), nil
+	return shim.Success([]byte("Invalid endpoint"))
 }
 
 func main() {}
